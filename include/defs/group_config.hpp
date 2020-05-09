456d2fa9b1c80523d56d1b5c300f67bb0e3df0a3
cyanray <a484599279@hotmail.com>
2020-05-09 19:33:40 +0800

    群设置类;

---
+++ include/defs/group_config.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#ifndef mirai_cpp_defs_group_config_hpp_H_
+#define mirai_cpp_defs_group_config_hpp_H_
+
+#include <nlohmann/json.hpp>
+#include "serializable.hpp"
+
+namespace Cyan
+{
+	// »∫…Ë÷√
+	class GroupConfig : public Serializable
+	{
+	public:
+		string Name;
+		string Announcement;
+		bool ConfessTalk;
+		bool AllowMemberInvite;
+		bool AutoApprove;
+		bool AnonymousChat;
+		virtual bool Set(const json& j) override
+		{
+			Name = j["name"].get<string>();
+			Announcement = j["announcement"].get<string>();
+			ConfessTalk = j["confessTalk"].get<bool>();
+			AllowMemberInvite = j["allowMemberInvite"].get<bool>();
+			AutoApprove = j["autoApprove"].get<bool>();
+			AnonymousChat = j["anonymousChat"].get<bool>();
+			return true;
+		}
+		virtual json ToJson() const override
+		{
+			json j = json::object();
+			j["name"] = Name;
+			j["announcement"] = Announcement;
+			j["confessTalk"] = ConfessTalk;
+			j["allowMemberInvite"] = AllowMemberInvite;
+			j["autoApprove"] = AutoApprove;
+			j["anonymousChat"] = AnonymousChat;
+			return j;
+		}
+	};
+
+}
+
+#endif // !mirai_cpp_defs_group_config_hpp_H_
