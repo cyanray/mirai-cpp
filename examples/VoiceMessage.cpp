cf00833a2350effecf0721615bd7bda08b1f08a5
cyanray <a484599279@hotmail.com>
2020-08-24 15:40:09 +0800

    新增: 语音消息示例;

---
+++ examples/VoiceMessage.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+// 使用静态库必须要在引入 mirai.h 前定义这个宏
+#define MIRAICPP_STATICLIB
+#include <mirai.h>
+
+int main()
+{
+	using namespace std;
+	using namespace Cyan;
+	system("chcp 65001");
+	MiraiBot bot("127.0.0.1", 539);
+	while (true)
+	{
+		try
+		{
+			bot.Auth("INITKEY7A3O1a9v", 1589588851_qq);
+			break;
+		}
+		catch (const std::exception& ex)
+		{
+			cout << ex.what() << endl;
+		}
+		MiraiBot::SleepSeconds(1);
+	}
+	cout << "成功登录 bot。" << endl;
+
+	bot.On<GroupMessage>(
+		[&](GroupMessage m)
+		{
+			MiraiVoice voice;
+			voice.Id = "BDD8FD5EAF989C4F4B7DB4D853A9509E.amr";
+			m.Reply(MessageChain().Voice(voice));
+		});
+
+
+	bot.EventLoop();
+
+	return 0;
+}
