bbd8a90ed5f146c22d974fcd56e90bf1bbf13cfc
cyanray <a484599279@hotmail.com>
2021-05-29 11:24:35 +0800

    更新 mirai/mirai.h.

--- include/mirai/mirai.h
+++ include/mirai/mirai.h
@@ -3,6 +3,7 @@
 #define mirai_cpp__mirai_h_H_
 
 #include "mirai/defs/defs.hpp"
+#include "mirai/messages/messages.hpp"
 #include "mirai/events/events.hpp"
 #include "mirai/exceptions/exceptions.hpp"
 #include "mirai/mirai_bot.hpp"
