747169597d5a0892c20ac0f0b1d7bd3c97117927
cyanray <a484599279@hotmail.com>
2021-05-29 10:42:58 +0800

    引入 magic_enum 库.

--- examples/RepeatMessage.cpp
+++ examples/RepeatMessage.cpp
@@ -55,7 +55,7 @@ int main(int argc, char* argv[])
 		[&](Message m)
 		{
 			cout << int64_t(m.Sender) << " 发来一条消息." << m.MessageChain.ToString() << endl;
-			m.Reply(MessageChain().Dice(1));
+			m.Reply(m.MessageChain);
 		});
 
 	bot.On<LostConnection>([&](LostConnection e)
