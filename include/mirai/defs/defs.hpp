00b3cd9c9de8e76b2017609100e3a51447aab0a8
cyanray <a484599279@hotmail.com>
2021-12-16 11:47:38 +0800

    重构: GitHub Actions CI 不通过.

--- include/mirai/defs/defs.hpp
+++ include/mirai/defs/defs.hpp
@@ -2,16 +2,15 @@
 #ifndef mirai_cpp_defs_defs_hpp_H_
 #define mirai_cpp_defs_defs_hpp_H_
 
-// 基础定义
 #include "ISerializable.hpp"
 #include "QQType.hpp"
-#include "Group.hpp"
 #include "Friend.hpp"
+#include "Group.hpp"
 #include "GroupMember.hpp"
-#include "MessageChain.hpp"
-#include "Profile.hpp"
 #include "GroupConfig.hpp"
 #include "GroupFile.hpp"
+#include "MessageChain.hpp"
+#include "Profile.hpp"
 #include "FileDownloadInfo.hpp"
 
 #endif // !mirai_cpp_defs_defs_hpp_H_
